98cf313a08141ff18dece541f84747aa4312c9d2
akozik <akozik@19c4192a-400a-11de-875d-dfced7ea1f7d>
2010-10-05 23:01:08 +0000

    new function - adaptor trimming

--- trunk/tcl_solexa_qseq_parser_2010_01_20.tcl
+++ trunk/tcl_solexa_qseq_parser_2010_01_20.tcl
@@ -6,6 +6,14 @@ proc Process_Qseq {argv} {
     set gc_lower 0.2
     set gc_upper 0.8
 
+	### ADAPTOR TRIMMING STATUS ###
+	set adaptor_trim "TRUE"
+	# set adaptor_trim "FALSE"
+	
+	### LOW COMPLEXITY TRIM ###
+	set lowcompl_trim "TRUE"
+	# set lowcompl_trim "FALSE"
+	
     ### INPUT - OUTPUT FILES ###
     set f_in1  [open [lindex $argv 0] "r"]
     set f_tab  [open [lindex $argv 1].trim.tab "w"]
@@ -23,7 +31,7 @@ proc Process_Qseq {argv} {
     while { [gets $f_in1 current_line] >= 0 } {
 	set current_data [split $current_line "\t"]
 	set machine [lindex $current_data  0]
-        set run_id  [lindex $current_data  1]
+    set run_id  [lindex $current_data  1]
 	set lane_n  [lindex $current_data  2]
 	set tile_n  [lindex $current_data  3]
 	set coordx  [lindex $current_data  4]
@@ -36,6 +44,7 @@ proc Process_Qseq {argv} {
 
 	### LIMIT SEQS TO SELECTED LENGTH ###
 	set seq_lmt [string  range  $seq_str  0  $r_len]
+	set seq_qlt [string  range  $seq_qlt  0  $r_len]
 
 	### SELECT ILLUMINA HQ FILTERED READS ONLY ###
 	if { $seq_flt == 1 } {
@@ -44,7 +53,44 @@ proc Process_Qseq {argv} {
 		set clean_length_q [string length $trimmed_quality]
 		### FIND AND REMOVE BAD BASECALLS ###
 		regsub -all {\..*} $seq_lmt "" trimmed_fasta
+		
+		set clean_length_t [string length $trimmed_fasta]
+		
+		### FIND SMALLEST ###
+		if { $clean_length_q <= $clean_length_t } {
+			set clean_length_a $clean_length_q
+		}
+		if { $clean_length_t <= $clean_length_q } {
+			set clean_length_a $clean_length_t
+		}
+		
+		### SELECT SUBSTRING ###
+		set trimmed_fasta [string range $seq_lmt  $tag_l  [expr $clean_length_a-1]]
+		
+		set adaptor_str ""
+		### ADAPTOR TRIMMING ###
+		if { $adaptor_trim == "TRUE" } {
+			set adp_before [string length $trimmed_fasta]
+			set trimmed_fasta [Adaptor_Trimming $trimmed_fasta]
+			set adp_after  [string length $trimmed_fasta]
+			if { $adp_after < $adp_before } {
+				set adaptor_str   " _ADP_TRM_ "
+			}
+		}
+		
+		set low_compl_str ""
+		### LOW COMPLEXITY TRIMMING ###
+		if { $lowcompl_trim == "TRUE" } {
+			set low_before [string length $trimmed_fasta]
+			set trimmed_fasta [LowCompl_Trimming $trimmed_fasta]
+			set low_after  [string length $trimmed_fasta]
+			if { $low_after < $low_before } {
+				set low_compl_str " LCMPL_TRM "
+			}
+		}
+		
 		set clean_length_s [string length $trimmed_fasta]
+		
 		### FIND SMALLEST ###
 		if { $clean_length_q <= $clean_length_s } {
 			set clean_length $clean_length_q
@@ -52,6 +98,7 @@ proc Process_Qseq {argv} {
 		if { $clean_length_s <= $clean_length_q } {
 			set clean_length $clean_length_s
 		}
+		
 		### SELECT SUBSTRING ###
 		set seq_trm [string range $seq_lmt  $tag_l  [expr $clean_length-1]]
 		set qlt_trm [string range $seq_qlt  $tag_l  [expr $clean_length-1]]
@@ -89,13 +136,13 @@ proc Process_Qseq {argv} {
 			### TAB-DELIMITED FILE ###
 			puts $f_tab "$l\t\ DATA_LINE_START @$fasta_id_str _SEQS_NEW_LINE_ $seq_trm\ +$fasta_id_str _QUAL_NEW_LINE_ $qlt_trm END_OF_DATA_LINE "
 			### FAST-Q FILE ###
-			puts $f_fsq "\@$fasta_id_str $l  TOT_LEN: $clean_length  \( $gc_status: $gc_length\/$trm_len \) "
+			puts $f_fsq "\@$fasta_id_str $l  TOT_LEN: $clean_length  \( $gc_status: $gc_length\/$trm_len \) $adaptor_str $low_compl_str"
 			puts $f_fsq $seq_trm
 			puts $f_fsq "\+"
 			puts $f_fsq $qlt_trm
 			puts $f_fsq ""
 			### FAST-A FILE ###
-			puts $f_fst "\>$fasta_id_str $l  TOT_LEN: $clean_length  \( $gc_status: $gc_length\/$trm_len \) "
+			puts $f_fst "\>$fasta_id_str $l  TOT_LEN: $clean_length  \( $gc_status: $gc_length\/$trm_len \) $adaptor_str $low_compl_str"
 			puts $f_fst $seq_trm
 			puts $f_fst ""
 			incr g
@@ -121,10 +168,35 @@ proc Process_Qseq {argv} {
     puts "DONE"
 }
 
+proc Adaptor_Trimming { trimmed_fasta } {
+	regsub -all -line {AGATCGGAAGAGCGGT.*} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGAAGAGCGGT$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGAAGAGCGG$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGAAGAGCG$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGAAGAGC$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGAAGAG$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGAAGA$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGAAG$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGAA$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {AGATCGGA$} $trimmed_fasta "" trimmed_fasta
+	return $trimmed_fasta
+}
+
+proc LowCompl_Trimming { trimmed_fasta } {
+	# regsub -all -line {^A{8,}} $trimmed_fasta "" trimmed_fasta
+	# regsub -all -line {^T{8,}} $trimmed_fasta "" trimmed_fasta
+	# regsub -all -line {^G{8,}} $trimmed_fasta "" trimmed_fasta
+	# regsub -all -line {^C{8,}} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {A{8,}$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {T{8,}$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {G{8,}$} $trimmed_fasta "" trimmed_fasta
+	regsub -all -line {C{8,}$} $trimmed_fasta "" trimmed_fasta
+	return $trimmed_fasta
+}
+
 if {$argc != 5} {
     puts "Program usage:"
     puts "qseq_file_to_process, output_file, tag_length, read_length, min_length_cutoff"
 } else {
     Process_Qseq $argv
 }
-
